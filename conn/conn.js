f5861335b99d31563242683b53ef387d7c43e0c6
vishnu prasanth <kvishnuprasanth2@gmail.com>
2024-07-16 12:27:40 +0530

    ok

--- conn/conn.js
+++ conn/conn.js
@@ -2,12 +2,13 @@ const mongoose = require("mongoose")
 const conn = async (res,req) =>{
     try {
         await mongoose.
-        connect("mongodb://127.0.0.1:27017/3wayassist",{
+        connect(process.env.MONGO_URL,{
             useNewUrlParser: true,
             useUnifiedTopology: true
         }).
         then(()=>{console.log("connected")})
     } catch (error) {
+        console.log(error)
         res.status(404).json({message:"not found"})
     }
 }
